1470ff572a2f91951f5bc9fd5fb881eb6804d190
marco-ih-bcn <marco.santonastasi@ironhack.com>
2021-06-21 13:13:02 +0200

    Done AJAX with fetch

--- 02_code-ajax_intro/index.js
+++ 02_code-ajax_intro/index.js
@@ -1,4 +1,4 @@
-const apiRequestPromise = fetch('https://api.spacexdata.com/v3/launches');
+const apiRequestPromise = fetch('http://api.spacexdata.com/v3/launches');
 
 
 apiRequestPromise
@@ -6,10 +6,27 @@ apiRequestPromise
   const data = response.json()
   return data
 })
-.then( data => {
-  console.log(data)
+.then( launch => {
+  launch.forEach(launch => {
+    if (launch.links.mission_patch) {
 
-  data.forEach(element => {
-    console.log(element)
+      const newImage = document.createElement('img');
+      newImage.setAttribute('src', launch.links.mission_patch);
+      newImage.setAttribute( 'width', 200);
+      
+      document.body.appendChild(newImage)
+    }
   });
+})
+.catch(error => {
+  const div = document.createElement('div');  
+  div.innerHTML = `
+    <div class="container error">
+      <h1>ERROR</h1>
+      <h2>${error}</h2>
+      <img src="https://vignette.wikia.nocookie.net/battlefordreamisland/images/f/f1/Roboty_book.png/revision/latest?cb=20190908174044" alt="">
+    </div>
+  `;
+
+  document.body.appendChild(div);
 })
